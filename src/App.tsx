a673525c0b53834cbd12bde01ee71763ce574cf6
Giovane Santos <giovane98sp@gmail.com>
2023-08-19 13:44:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as C from './App.style'
+
+export const App = () => {
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        aaa
+      </C.Body>
+    </C.Container>
+  )
+}
